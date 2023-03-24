b8dab0b77f2fd6225a191442f56533311c4cd7e3
Madhan S <madhan@marmeto.com>
2023-03-24 13:24:14 +0530

    helloworld debugg

--- src/api.js
+++ src/api.js
@@ -8,35 +8,35 @@ const router = express.Router();
 
 // Define a route that responds with a JSON object when a GET request is made to the root path
 router.get("/", (req, res) => {
-  if (req.query.device === 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiRGhhbnVzaCIsImlhdCI6MTY3OTQ2OTY3OX0.zbzyyzkiB0M0bUZs_ydjPaGpM2PbPbJ1cqi-PTLiS3c') {
-    let data = JSON.stringify({
-      "theme": {
-        "role": "main"
-      }
-    });
+  // if (req.query.device === 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiRGhhbnVzaCIsImlhdCI6MTY3OTQ2OTY3OX0.zbzyyzkiB0M0bUZs_ydjPaGpM2PbPbJ1cqi-PTLiS3c') {
+  //   let data = JSON.stringify({
+  //     "theme": {
+  //       "role": "main"
+  //     }
+  //   });
 
-    let config = {
-      method: 'put',
-      maxBodyLength: Infinity,
-      url: 'https://madhans-area-51.myshopify.com/admin/api/2023-01/themes/121517047870.json',
-      headers: {
-        'X-Shopify-Access-Token': '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',
-        'Content-Type': 'application/json',
-        'Cookie': '_secure_admin_session_id=0ee4728e993f9ea116f7dd25d1053bba; _secure_admin_session_id_csrf=0ee4728e993f9ea116f7dd25d1053bba'
-      },
-      data: data
-    };
+  //   let config = {
+  //     method: 'put',
+  //     maxBodyLength: Infinity,
+  //     url: 'https://madhans-area-51.myshopify.com/admin/api/2023-01/themes/121517047870.json',
+  //     headers: {
+  //       'X-Shopify-Access-Token': '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',
+  //       'Content-Type': 'application/json',
+  //       'Cookie': '_secure_admin_session_id=0ee4728e993f9ea116f7dd25d1053bba; _secure_admin_session_id_csrf=0ee4728e993f9ea116f7dd25d1053bba'
+  //     },
+  //     data: data
+  //   };
 
-    axios.request(config)
-      .then((response) => {
-        console.log(JSON.stringify(response.data));
-        res.send(JSON.stringify(response.data));
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }
-  // res.send('funcion exectued');
+  //   axios.request(config)
+  //     .then((response) => {
+  //       console.log(JSON.stringify(response.data));
+  //       res.send(JSON.stringify(response.data));
+  //     })
+  //     .catch((error) => {
+  //       console.log(error);
+  //     });
+  // }
+  res.send(JSON.stringify({hello:"world"}));
 });
 
 // Use the router to handle requests to the `/.netlify/functions/api` path
